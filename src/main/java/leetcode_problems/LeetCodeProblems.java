bbdd2a468b559e6fbc70ae4629e648ef4a61023d
Davronbek Normamatov <davronnormamatov004@gmail.com>
2024-10-27 17:45:01 +0500

    commit

--- src/main/java/leetcode_problems/LeetCodeProblems.java
+++ src/main/java/leetcode_problems/LeetCodeProblems.java
@@ -666,11 +666,11 @@ public class LeetCodeProblems {
         int min = prices[0];
         int profit = 0;
         int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
             } else {
-                profit = prices[i] - min;
+                profit = price - min;
                 if (profit > maxProfit)
                     maxProfit = profit;
             }
